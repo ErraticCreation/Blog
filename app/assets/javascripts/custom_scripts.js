baa79f83385daeb492c98a21e910991c4f238420
Tarah Nixon <erraticcreation@gmail.com>
2016-05-03 15:29:41 -0400

    Reworked the layout to include a togglable sidebar to be populated specifically with blog post navigation

--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -14,4 +14,8 @@ $(document).ready(function(){
 
   $('body').scrollspy({ target: '#navbar' });
 
+  $('#toggle-menu').click(function (e) {
+    e.preventDefault();
+    $('#page-wrapper').toggleClass('menu-displayed');
+  });
 });

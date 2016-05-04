adfa41dc2030d71dcda54b3cbf729dd4926a9fbd
Tarah Nixon <erraticcreation@gmail.com>
2016-05-04 16:38:10 -0400

    Restyled root path, implemented image uploads, and generated devise user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  mount_uploader :picture, PictureUploader
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true
+  validate :picture_size
+
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end

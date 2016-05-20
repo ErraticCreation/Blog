d93a7d51fc8d7bb7fa3e4545488322f2af7dc946
Tarah Nixon <erraticcreation@gmail.com>
2016-05-19 22:03:46 -0400

    Updated comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.post_id = params[:post_id]
-    @comment.author_name = current_user.first_name
+    @comment.author_name = "#{current_user.first_name} #{current_user.last_name}"
 
     @comment.save
 

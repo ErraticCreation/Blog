527c981be53041b10620f392839865a274e1bdc3
Tarah Nixon <erraticcreation@gmail.com>
2016-05-24 00:36:37 -0400

    Fixed minor authentication bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:show, :index]
+  before_action :authenticate_user!, :except => [:show, :index, :archives]
   before_filter :nav_post_month, :only => :read
   load_and_authorize_resource
 
